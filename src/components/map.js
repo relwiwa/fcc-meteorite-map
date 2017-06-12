c22c4ae190bd21e350f9ce478cae43e80303ba96
Walter <relw1wa-github@yahoo.de>
2017-06-12 14:30:44 +0200

    Setup world map from topojson files

--- src/components/map.js
+++ src/components/map.js
@@ -1,14 +1,61 @@
+/*  Creating a d3 map within react, adapted from:
+    - https://bost.ocks.org/mike/map
+    - https://medium.com/@zimrick/how-to-create-pure-react-svg-maps-with-topojson-and-d3-geo-e4a6b6848a98 */
+
 import React, { Component } from 'react';
+import { geoMercator as d3GeoMercator, geoPath as d3GeoPath } from 'd3-geo';
+import { feature } from 'topojson-client';
+
+/*  Origin of data:
+    - SHP-file from http://www.naturalearthdata.com
+    - Converted using ogr2ogr to geoJson and then to topoJson */
+import topoJsonData from '../data/countries.topo.json';
 
 const chartHeight = 600;
 const chartWidth = 800;
 
 class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countriesData: [],
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      countriesData: feature(topoJsonData, topoJsonData.objects.units).features,
+    });
+  }
+
+  setupMap() {
+    return (
+      d3GeoMercator()
+        .scale(100)
+        .translate([chartWidth / 2, chartHeight /2])
+    );
+  }
 
   render() {
+    const { strikeData } = this.props;
+    const { countriesData } = this.state;
+
      return (
       <div className="map" style={{height:chartHeight, position: 'relative', width: chartWidth}}>
-        <svg style={{background: 'lightgray', height: chartHeight, width: chartWidth}}></svg>
+        <svg style={{background: 'lightgray', height: chartHeight, width: chartWidth}}>
+          <g>
+            {countriesData.map((countryDatum, index) => (
+              <path
+                // index as key is ok, as topojson data is served from static file
+                key={index}
+                d={d3GeoPath(this.setupMap())(countryDatum)}
+                fill="lightblue"
+                stroke="gray"
+                strokeWidth="0.5"
+              />
+            ))}
+          </g>
+        </svg>
       </div>
     );
   }
--- src/components/meteorite-map.js
+++ src/components/meteorite-map.js
@@ -1,19 +1,56 @@
+import axios from 'axios';
+import es6Promise from 'es6-promise';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import Map from './map';
 
-import strikeData from '../data/meteorite-strike-data.json';
+es6Promise.polyfill();
+const axiosConfig = axios.create({
+  timeout: 1000
+});
 
 class MeteoriteMap extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      strikeData: [],
+    }
+  }
+
+  componentWillMount() {
+    this.getStrikeData();
+  }
+
+  getStrikeData() {
+    axios.get('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json')
+    .then((data) => {
+      const strikeData = this.prepareStrikeData(data.data);
+      this.setState({
+        strikeData: strikeData,
+      })
+    });
+  }
+
+  prepareStrikeData(data) {
+    let strikeData = [];
+    data.features.map((feature) => {
+      strikeData.push(feature);
+    });
+    return strikeData;   
+  }
+  
   render() {
+    const { strikeData } = this.state;
 
     return (
       <div className="meteorite-map row">
         <div className="column small-12">
           <h1 className="text-center">Map of Meteorite Landings Across the World</h1>
-            <Map />
+            <Map
+              strikeData={strikeData}
+            />
         </div>
       </div>
     );
