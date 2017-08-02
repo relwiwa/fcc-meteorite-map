9a2d5dc94beb841af8ae1da380eccfaac85c5de0
Walter <relw1wa-github@yahoo.de>
2017-08-02 18:40:18 +0200

    Update dependencies, babel and webpack config, npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,10 @@ const config = {
       {
         use: 'babel-loader',
         test: /\.js$/,
-        exclude: /node_modules/
+        include: [
+          path.resolve(__dirname, 'src'),
+          path.resolve(__dirname, 'node_modules/foundation-sites/js')
+        ]
       },
       {
         loader: ExtractTextPlugin.extract({
